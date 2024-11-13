0d502a09a4dfa53f681513dd695bda94180e66eb
navanee1609 <navaneethanvs18@gmail.com>
2024-11-13 21:19:59 +0530

    added tewMerge for the multiple classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
-"use client"
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, Calistoga } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Navaneethan _ Front-End Developer",
@@ -14,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={twMerge(inter.className, calistoga.className, "bg-gray-900 text-white antialiased")}>
+        {children}
+      </body>
     </html>
   );
 }
