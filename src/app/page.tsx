4464a6ac5077c472f782198c74462c4042e1bd0f
navanee1609 <navaneethanvs18@gmail.com>
2024-11-16 12:12:17 +0530

    added tape section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
      <HeroSection/>
      {/* importing projects section */}
      <ProjectsSection/>
+     {/*Importing Tape Section  */}
+     <TapeSection/>
     </div>
   );
 }
