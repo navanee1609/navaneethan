d548ced5642e025dfa00f4e62004941d7564feb7
navanee1609 <navaneethanvs18@gmail.com>
2024-11-16 09:54:02 +0530

    added hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="fixed top-3 left-0 right-0 flex justify-center items-center">
+    <div className="fixed top-3 left-0 right-0 flex justify-center items-center w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">Home</a>
         <a href="#" className="nav-item">Project</a>
